5182acfe0fb162d74600d468b57180da9a424eff
TiiGeeRTaeM <36447221+TiiGeeRTaeM@users.noreply.github.com>
2018-02-13 19:36:59 +0330

    Add files via upload

---
+++ cli/data/config.lua
@@ -0,0 +1,24 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "BanHammer",
+    "Fun",
+    "GroupManager",
+    "Msg-Checks",
+    "Plugins",
+    "Tools",
+    "Write"
+  },
+  info_text = "\9》TiiGeR BoT v5.7\nAn advanced administration bot based on https://valtman.name/telegram-cli\n\n》https://github.com/BeyondTeam/BDReborn \n\n》Admins :\n》@Arashwm ➣ Founder & Developer《\n》@JaviadSudo ➣ Developer《\n》@Shanisloop ➣ Developer《\n\n》Special thanks to :\n》TiGeRTeaM\n》@Xamcrin_Devloper\n\n》Our channel :\n》@TiGeRTeam《\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    205549111,
+    205549111,
+    205549111
+  }
+}
+return _
+end
